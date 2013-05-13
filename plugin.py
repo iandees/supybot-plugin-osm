23582af6e8ea176da98c7f60934afb78d082025f
Ian Dees <ian.dees@gmail.com>
2013-05-13 11:07:15 -0500

    Add some debug logging for short geocodes.

--- plugin.py
+++ plugin.py
@@ -244,6 +244,9 @@ class OSM(callbacks.Plugin):
             location = " near %s" % (location)
             location = location.encode('utf-8')
 
+        if len(location) > 0 and len(location) < 26:
+            log.info('Location "%s" was too short. Response to %s was: %s' % (location, url, json.dumps(info)))
+
         return (country_code, location)
 
     def _notes_rss_poll(self):
